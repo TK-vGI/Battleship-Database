d85d9364e49a15ec2a8e6491704f3d3e95b60e86
Tomáš Kaňuch <tomas.kanuch@gmail.com>
2026-01-17 22:53:50 +0100

    Stage 1: SELECT,FROM,WHERE,ORDER BY

---
+++ Query_Stage1.sql
@@ -0,0 +1,41 @@
+/*
+START STAGE 1
+*/
+SELECT ship, result
+FROM Outcomes
+WHERE battle = 'Pearl Harbor'
+ORDER BY ship;
+    
+/*
+END STAGE 1
+*/
+
+/*
+Other Solution
+--
+
+ */
+
+/*
+Other Solution
+--
+
+ */
+
+/*
+Other Solution
+--
+
+ */
+
+/*
+Other Solution
+--
+
+ */
+
+/*
+Other Solution
+--
+
+ */
